2275a6d2867c25a7e9ab7179bf87e8e02e33d625
SooluThomas <soolu.elto@gmail.com>
2019-09-10 19:24:19 -0400

    Update deploy_documentation.sh

--- tools/deploy_documentation.sh
+++ tools/deploy_documentation.sh
@@ -22,7 +22,7 @@ SOURCE_DOC_DIR="docs/_build/html/locale"
 SOURCE_DIR=`pwd`
 TRANSLATION_LANG='ja'
 
-make autodoc
+cd docs
 
 # Setup the deploy key.
 # https://gist.github.com/qoomon/c57b0dc866221d91704ffef25d41adcf
