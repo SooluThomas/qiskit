e465cba4161a6a621ce4cf207aa14c9567cc4300
SooluThomas <soolu.elto@gmail.com>
2019-09-10 19:07:18 -0400

    Update deploy_documentation.sh

--- tools/deploy_documentation.sh
+++ tools/deploy_documentation.sh
@@ -36,7 +36,7 @@ ssh-add github_deploy_key
 echo "end of configuring ssh"
 
 # Clone the sources and po files
-git clone $SOURCE_REPOSITORY docs_source
+git clone $SOURCE_REPOSITORY docs_source --single-branch --branch translationDocs
 cp -r docs_source/docs/. $SOURCE_DIR/docs/
 
 echo "pwd before cd docs"
